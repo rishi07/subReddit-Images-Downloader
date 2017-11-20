98c9a16c89c38bc3c8881e6eed202a02ab2a4f4d
Vishva Nath Dutt Sharma <visitdutt@gmail.com>
2017-11-21 00:12:39 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+# dependencies
+# pip install praw
+# pip install wget
+
 import os
 import praw
 import re
 import sys
 import requests 
 import urllib
+import urllib2
 from subprocess import call
 import wget
-from time import sleep
-import pycurl
-
+import time
 
 reddit = praw.Reddit(client_id = '<insert client id>',
 				     client_secret = '<insert client secret>',
@@ -16,30 +19,38 @@ reddit = praw.Reddit(client_id = '<insert client id>',
 				     username = '<insert username here>',
 				     password = '<insert password here>' 
 				     )
-
-print ("Enter name of subreddit ")
-subr = input()
+# print (reddit.read_only) 
+subr = str(raw_input())
 pkp =  os.getcwd()
-
 
 if not os.path.exists(subr):
     os.makedirs(subr)
 
 
 folderpath = str(pkp) +'/'+ subr +'/'
+# print folderpath
 os.chdir(folderpath) 
 
-
-
 i = 1
-for submission in reddit.subreddit(subr).hot(limit=None):
-	url = submission.url
-	if '/i.' in url:
-		# result = urllib.urlretrieve(url,'/media/user/X/')
-		filename = wget.download(url)
-		print("\n saved ",i)
-		print ("\n")
-		i+=1
+pkp = list()
+for submission in reddit.subreddit(subr).top(limit=None):
 
-		
-		
+	url2 = submission.url
+	if '/i.' in url2:
+		f = urllib2.urlopen(url2)
+		f2 = str(url2)
+		extention = f2.split(".")
+		extentionstr = str(extention[-1])
+		extentionstr = extentionstr.strip('/\\')
+		fname = str(i)+"."+extentionstr	
+		print fname
+		try:
+			with open(fname,"wb") as img:
+				img.write(f.read())
+		except:
+			print "some error encountered :D \n\n"
+		print url2
+		print i,"^---\n"
+		i+=1
+		if(i%20==0):
+			time.sleep(2)
